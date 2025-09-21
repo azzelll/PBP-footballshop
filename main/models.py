2980a5b52cbb1aa667368ee95aaed4aa456880db
azzelll <madeshandykrisnanda@gmail.com>
2025-09-21 23:01:27 +0700

    Finish Tugas 4

--- main/models.py
+++ main/models.py
@@ -1,5 +1,6 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import User
 
 class Size(models.Model):
     name = models.CharField(max_length=10)
@@ -12,7 +13,7 @@ class Product(models.Model):
         ('accessories', 'Aksesoris'),
         ('lifestyle', 'Lifestyle / Merchandise'),
     ]
-
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True) 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     brand = models.CharField(max_length=50)
