76ba7ad1e04fc004a6f37a236a4679d833a9b7fa
azzelll <madeshandykrisnanda@gmail.com>
2025-10-06 23:48:51 +0700

    cp t6

--- main/urls.py
+++ main/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
-from main.views import show_main, create_product, show_product, show_xml, show_json, show_json_by_id, show_xml_by_id, register, login_user, logout_user, edit_product, delete_product
+from main.views import (
+    show_main, create_product, show_product, show_xml, show_json, 
+    show_json_by_id, show_xml_by_id, register, login_user, logout_user, 
+    edit_product, delete_product,
+    # AJAX endpoints
+    create_product_ajax, delete_product_ajax, update_product_ajax, get_products_json,
+    login_ajax, register_ajax
+)
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
     path('create-product/', create_product, name='create_product'),
-    path('product/<str:id>/', show_product, name='show_product'),
+    path('product/<uuid:id>/', show_product, name='show_product'),
     path('xml/', show_xml, name='show_xml'),
     path('json/', show_json, name='show_json'),
     path('xml/<str:Product_id>/', show_xml_by_id, name='show_xml_by_id'),
@@ -16,4 +23,12 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('product/<uuid:id>/edit/', edit_product, name='edit_product'),
     path('product/<uuid:id>/delete/', delete_product, name='delete_product'),
+    
+    # AJAX endpoints
+    path('api/products/', get_products_json, name='get_products_json'),
+    path('api/create-ajax/', create_product_ajax, name='create_product_ajax'),
+    path('api/update-ajax/<uuid:id>/', update_product_ajax, name='update_product_ajax'),
+    path('api/delete-ajax/<uuid:id>/', delete_product_ajax, name='delete_product_ajax'),
+    path('api/login/', login_ajax, name='login_ajax'),
+    path('api/register/', register_ajax, name='register_ajax'),
 ]
